e479f80e36cf9de935bdad14274bb48011833947
devpatelwebdeveloper <hello@devashishpatel.com>
2019-11-09 14:39:33 -0500

    Updated Section to be dynamic

--- src/components/blocks/CourseCards/CourseCards.js
+++ src/components/blocks/CourseCards/CourseCards.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
 import Section from "../../organisms/Section/Section";
 import Row from "react-bootstrap/Row";
-import styled from "styled-components";
 import CourseCard from "../../molecules/CourseCard/CourseCard";
 import { Courses } from "../../../contents/Courses";
 
 export default function CourseCards() {
   return (
-    <Section>
+    <Section padding="20px" margin="50px">
       <Row>
         {Courses.map((course) => (
           <CourseCard

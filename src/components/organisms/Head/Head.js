19fcce07765b50602649eadd09c53b3a4fcd24df
devpatelwebdeveloper <hello@devashishpatel.com>
2019-11-15 14:45:58 -0500

    Updated meta Desc

--- src/components/organisms/Head/Head.js
+++ src/components/organisms/Head/Head.js
@@ -17,31 +17,25 @@ const Head = ({ title }) => {
     <Helmet title={`${title} | ${data.site.siteMetadata.title}`}>
       <meta
         name="description"
-        content="Devashish Patel | Front End Web Developer based in TO"
+        content="TrainingHub | Lean your career in IT with TrainingHub.io"
       />
 
       <meta property="og:locale" content="en_US" />
       <meta property="og:type" content="website" />
-      <meta
-        property="og:title"
-        content="Devashish Patel | Front End Web Developer based in TO"
-      />
+      <meta property="og:title" content="TrainingHub | Your gateway to IT" />
       <meta
         property="og:description"
-        content="Hello! Good to see you here, I am Frontend Web Developer based in Toronto Canada"
+        content="TrainingHub.io helps talented professionals and developers find success in IT careers they love through community-oriented classroom training and personalized support."
       />
       <meta property="og:url" content={url} />
-      <meta property="og:site_name" content="Dev Patel Web Developer" />
+      <meta property="og:site_name" content="TrainingHub.io" />
       <meta property="og:image" content={`${url}`} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta
         name="twitter:description"
-        content="Hello! Good to see you here, I am Frontend Web Developer based in Toronto Canada"
+        content="TrainingHub.io helps talented professionals and developers find success in IT careers they love through community-oriented classroom training and personalized support."
       />
-      <meta
-        name="twitter:title"
-        content="Devashish Patel | Front End Web Developer based in TO"
-      />
+      <meta name="twitter:title" content="TrainingHub | Your gateway to IT" />
       <meta name="twitter:image" content={`${url}`} />
       <link
         href="https://fonts.googleapis.com/css?family=Merriweather:300,300i,400,400i,700,700i,900,900i&display=swap"

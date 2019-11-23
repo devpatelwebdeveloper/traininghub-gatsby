769a64b1cf1579ad7eaafaa97e6730772c314e44
devpatelwebdeveloper <hello@devashishpatel.com>
2019-11-23 00:13:01 -0500

    Accordion Created

---
+++ src/components/blocks/Accordion/Accordion.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import Styles from "../../../styles/Styles";
+import AccordionItem from "../../molecules/AccordionItem/AccordionItem";
+
+const AccordionList = styled.ul`
+  list-style: none;
+  margin: 5px;
+  padding: 0;
+  background-color: transparent;
+  max-width: 100%;
+  overflow: hidden;
+  box-shadow: 0 0 0.8rem 0.1rem
+      rgba(darken(${Styles.Colors.BaseLightBlue}, 20%), 0.06),
+    0 20px 30px -10px rgba(darken(${Styles.Colors.BaseLightBlue}, 20%), 0.2);
+`;
+const AccordionSingleList = styled.li`
+  border-top: 1px solid ${Styles.Colors.BaseBorderGrey};
+`;
+
+const paragraph =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus sint provident vel ab reprehenderit cum soluta.";
+
+const data = [
+  {
+    title: "Pricing plans Teast",
+    paragraph,
+  },
+  {
+    title: "How to apply asd",
+    paragraph,
+  },
+  {
+    title: "Purchasing process",
+    paragraph,
+  },
+  {
+    title: "Usage guides",
+    paragraph,
+  },
+];
+
+class Accordion extends React.Component {
+  render() {
+    return (
+      <AccordionList>
+        {data.map((data, key) => {
+          return (
+            <AccordionSingleList {...key}>
+              <AccordionItem {...data} />
+            </AccordionSingleList>
+          );
+        })}
+      </AccordionList>
+    );
+  }
+}
+
+export default Accordion;
--- src/pages/courses/microsofttechnologies/index.js
+++ src/pages/courses/microsofttechnologies/index.js
@@ -5,6 +5,7 @@ import TopBannerCourse from "../../../components/organisms/TopBannerCourse/TopBa
 import ContentGenericAligned from "../../../components/blocks/ContentGenericAligned/ContentGenericAligned";
 import BaseTitle from "../../../components/atoms/BaseTitle/BaseTitle";
 import StudentJourney from "../../../components/blocks/StudentJourney/StudentJourney";
+import Accordion from "../../../components/blocks/Accordion/Accordion";
 
 export default function CourseSingle() {
   return (
@@ -35,6 +36,7 @@ export default function CourseSingle() {
           imageTwo="https://unsplash.it/1500?random"
           imageTwoAlt="Image Two Text"
         />
+        <Accordion />
       </Layout>
     </>
   );

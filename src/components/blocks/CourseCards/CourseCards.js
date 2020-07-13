2ce53397c8085564dcd5e7b23e8ab81429d399cd
Devashish Patel <dev.patel8787@gmail.com>
2020-07-12 20:37:13 -0400

    random course

--- src/components/blocks/CourseCards/CourseCards.js
+++ src/components/blocks/CourseCards/CourseCards.js
@@ -6,6 +6,27 @@ import { Courses } from "../../../contents/Courses";
 import BaseTitle from "../../atoms/BaseTitle/BaseTitle";
 
 export default function CourseCards() {
+  function shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+  // console.log(Courses);
+  const randCourse = shuffle(Courses);
   return (
     <Section padding="20px" margin="50px">
       <BaseTitle
@@ -15,7 +36,7 @@ export default function CourseCards() {
         underline
       />
       <Row>
-        {Courses.map((course) => (
+        {randCourse.map((course) => (
           <CourseCard
             key={course.subtitle}
             title={course.title}

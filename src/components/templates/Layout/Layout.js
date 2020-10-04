b93b43817837ff2740e1f2c38d2bb9e8aa76007a
Devashish Patel <dev.patel8787@gmail.com>
2020-10-04 15:08:20 -0400

    removed ability to select
    !

--- src/components/templates/Layout/Layout.js
+++ src/components/templates/Layout/Layout.js
@@ -1,16 +1,29 @@
 import React from "react";
+import styled, { css } from "styled-components";
 import Navigation from "../../organisms/Navigation/Navigation";
 import Footer from "../../organisms/Footer/Footer";
 import ScrollToTop from "../../atoms/ScrollToTop/ScrollToTop";
 
+const StyledLayout = styled.div`
+  @media print {
+    display: none;
+  }
+  -webkit-touch-callout: none !important;
+  -webkit-user-select: none !important;
+  -khtml-user-select: none !important;
+  -moz-user-select: none !important;
+  -ms-user-select: none !important;
+  user-select: none !important;
+`;
+
 const Layout = (props) => {
   return (
-    <>
+    <StyledLayout>
       <Navigation />
       {props.children}
       <ScrollToTop />
       <Footer />
-    </>
+    </StyledLayout>
   );
 };
 export default Layout;

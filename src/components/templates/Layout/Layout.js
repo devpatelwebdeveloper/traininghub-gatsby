8f75e7675e3391b4378d85bcdc676e07cd016535
Devashish Patel <dev.patel8787@gmail.com>
2020-10-11 11:13:00 -0400

    remove copy from layout

--- src/components/templates/Layout/Layout.js
+++ src/components/templates/Layout/Layout.js
@@ -4,26 +4,14 @@ import Navigation from "../../organisms/Navigation/Navigation";
 import Footer from "../../organisms/Footer/Footer";
 import ScrollToTop from "../../atoms/ScrollToTop/ScrollToTop";
 
-const StyledLayout = styled.div`
-  @media print {
-    display: none;
-  }
-  -webkit-touch-callout: none !important;
-  -webkit-user-select: none !important;
-  -khtml-user-select: none !important;
-  -moz-user-select: none !important;
-  -ms-user-select: none !important;
-  user-select: none !important;
-`;
-
 const Layout = (props) => {
   return (
-    <StyledLayout>
+    <>
       <Navigation />
       {props.children}
       <ScrollToTop />
       <Footer />
-    </StyledLayout>
+    </>
   );
 };
 export default Layout;

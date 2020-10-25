600eacfddef623d9a086ed73e66eb5ae1c51d18f
Devashish Patel <dev.patel8787@gmail.com>
2020-10-24 22:52:38 -0400

    Filter added

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Head from "../components/organisms/Head/Head";
 import Layout from "../components/templates/Layout/Layout";
 import Section from "../components/organisms/Section/Section";
@@ -12,6 +12,9 @@ import BlogItem from "../components/molecules/BlogItem/BlogItem";
 import { graphql, useStaticQuery } from "gatsby";
 
 export default function AboutPage() {
+  const [filter, setFilter] = useState("all");
+  const [projects, setProjects] = useState([]);
+
   const blogs = useStaticQuery(graphql`
     query {
       allContentfulBlogs {
@@ -33,6 +36,46 @@ export default function AboutPage() {
     }
   `);
 
+  //test
+  const portfolio = [
+    {
+      name: "My best client",
+      category: ["all", "frontend", "ux-ui"],
+    },
+    {
+      name: "My favorite case",
+      category: ["all", "mobile", "ux-ui"],
+    },
+    {
+      name: "A old job",
+      category: ["all", "frontend"],
+    },
+    {
+      name: "It is a really cool website",
+      category: ["all", "frontend", "ux-ui"],
+    },
+    {
+      name: "Something more",
+      category: ["all", "others"],
+    },
+  ];
+
+  const port = blogs.allContentfulBlogs.edges;
+  //test
+  useEffect(() => {
+    setProjects(portfolio);
+  }, []);
+
+  useEffect(() => {
+    setProjects([]);
+
+    const filtered = portfolio.map((p) => ({
+      ...p,
+      filtered: p.category.includes(filter),
+    }));
+    setProjects(filtered);
+  }, [filter]);
+
   return (
     <>
       <Head title="Blogs" />
@@ -42,6 +85,64 @@ export default function AboutPage() {
           text="Explore insights, tips, and articles written by experts in a range of professional domains."
           background={Background}
         />
+        <Section marginTop="50px" marginBottom="25px">
+          <div className="portfolio__labels">
+            <a
+              href="/#"
+              active={filter === "all"}
+              onClick={(e) => {
+                e.preventDefault();
+                setFilter("all");
+              }}>
+              All
+            </a>
+            <a
+              href="/#"
+              active={filter === "frontend"}
+              onClick={(e) => {
+                e.preventDefault();
+                setFilter("frontend");
+              }}>
+              Frontend
+            </a>
+            <a
+              href="/#"
+              active={filter === "mobile"}
+              onClick={(e) => {
+                e.preventDefault();
+                setFilter("mobile");
+              }}>
+              Mobile
+            </a>
+            <a
+              href="/#"
+              active={filter === "ux-ui"}
+              onClick={(e) => {
+                e.preventDefault();
+                setFilter("ux-ui");
+              }}>
+              UX/UI
+            </a>
+            <a
+              href="/#"
+              active={filter === "others"}
+              onClick={(e) => {
+                e.preventDefault();
+                setFilter("others");
+              }}>
+              Others
+            </a>
+          </div>
+          <div className="portfolio__container">
+            {projects.map((item) =>
+              item.filtered === true ? (
+                <span key={item.name}>{item.name}</span>
+              ) : (
+                ""
+              ),
+            )}
+          </div>
+        </Section>
         <Section marginTop="50px" marginBottom="25px">
           <Row>
             {blogs.allContentfulBlogs.edges.map((blog) => {

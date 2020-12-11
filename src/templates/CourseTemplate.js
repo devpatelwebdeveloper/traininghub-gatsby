bd1bcd5df43dbcf65b397ca9b078364644f632cb
Devashish Patel <dev.patel8787@gmail.com>
2020-12-10 22:29:15 -0500

    Riche Text Function working

--- src/templates/CourseTemplate.js
+++ src/templates/CourseTemplate.js
@@ -1,7 +1,59 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import styled from "styled-components";
+import Styles from "../styles/Styles";
+import Head from "../components/organisms/Head/Head";
+import Layout from "../components/templates/Layout/Layout";
+import Section from "../components/organisms/Section/Section";
+import TopBannerCourse from "../components/organisms/TopBannerCourse/TopBannerCourse";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { RichTextOptions } from "../utilities/richtextFunction";
 
-const Course = () => {
-  return <>Course Template</>;
+export const query = graphql`
+  query($slug: String!) {
+    contentfulCourseContent(slug: { eq: $slug }) {
+      title
+      courseIntroduction {
+        json
+      }
+      topBannerImage {
+        file {
+          url
+        }
+      }
+    }
+  }
+`;
+
+const Course = (props) => {
+  const Course = props.data.contentfulCourseContent;
+  return (
+    <>
+      <Head title={Course.title} />
+      <Layout>
+        <TopBannerCourse
+          courseTitle={Course.title}
+          text={documentToReactComponents(
+            Course.courseIntroduction.json,
+            RichTextOptions,
+          )}
+          courseImage={Course.topBannerImage.file.url}
+        />
+        <Section marginTop="50px" marginBottom="25px">
+          <Row>
+            <Col md={12}>
+              {documentToReactComponents(
+                Course.courseIntroduction.json,
+                RichTextOptions,
+              )}
+            </Col>
+          </Row>
+        </Section>
+      </Layout>
+    </>
+  );
 };
 
 export default Course;
---
+++ src/utilities/richtextFunction.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import { BLOCKS, MARKS } from "@contentful/rich-text-types";
+
+import Paragraph from "../components/atoms/Paragraph/Paragraph";
+import BaseTitle from "../components/atoms/BaseTitle/BaseTitle";
+
+const RichTextParagraph = ({ children }) => <Paragraph>{children}</Paragraph>;
+const RichTextTitle = ({ children, size }) => (
+  <BaseTitle title={children} size={size} />
+);
+
+const RichTextOptions = {
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (node, children) => (
+      <RichTextParagraph>{children}</RichTextParagraph>
+    ),
+    [BLOCKS.HEADING_1]: (node, children) => (
+      <RichTextTitle size="H1">{children}</RichTextTitle>
+    ),
+    [BLOCKS.HEADING_2]: (node, children) => (
+      <RichTextTitle size="H2">{children}</RichTextTitle>
+    ),
+    [BLOCKS.HEADING_3]: (node, children) => (
+      <RichTextTitle size="H3">{children}</RichTextTitle>
+    ),
+    [BLOCKS.HEADING_4]: (node, children) => (
+      <RichTextTitle size="H4">{children}</RichTextTitle>
+    ),
+    [BLOCKS.HEADING_5]: (node, children) => (
+      <RichTextTitle size="H5">{children}</RichTextTitle>
+    ),
+    [BLOCKS.HEADING_6]: (node, children) => (
+      <RichTextTitle size="H6">{children}</RichTextTitle>
+    ),
+    "embedded-asset-block": (node) => {
+      const alt = node.data.target.fields.title["en-US"];
+      const url = node.data.target.fields.file["en-US"].url;
+      return (
+        <img
+          alt={alt}
+          src={url}
+          style={{
+            maxWidth: "100%",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        />
+      );
+    },
+  },
+};
+
+export { RichTextOptions };

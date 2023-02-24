dd1e719f808963d6ddeda0bcfa0a0fa648138d20
Devashish Patel <dev.patel8787@gmail.com>
2023-02-24 13:15:08 -0500

    removed extra consoles

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,12 +25,10 @@ plugins.push(
   {
     resolve: `gatsby-source-contentful`,
     options: {
-      spaceId:"dl64kxf2oxn0",
-      accessToken:'RCTh5YsftNWOyLc_o7uuSQ0e7dRceD6A6rVWIgAweTg'
 
-      // spaceId: process.env.CONTENTFUL_SPACE_ID,
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
       // Learn about environment variables: https://gatsby.dev/env-vars
-      // accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
     },
   },
 );
